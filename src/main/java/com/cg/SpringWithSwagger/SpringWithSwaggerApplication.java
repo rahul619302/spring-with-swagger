801584535e5b7bd0c52f3311557425302c0015d4
rahul619302 <rahul.ab.singh@capgemini.com>
2018-11-06 16:10:36 +0530

    remove hello world

--- src/main/java/com/cg/SpringWithSwagger/SpringWithSwaggerApplication.java
+++ src/main/java/com/cg/SpringWithSwagger/SpringWithSwaggerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringWithSwaggerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Check Git Change Commit");
 		SpringApplication.run(SpringWithSwaggerApplication.class, args);
 	}
 }
